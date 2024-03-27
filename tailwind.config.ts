7148673b2d55b7a1bef78ee501a3bfaa31f3f23d
Psychless <kristapsbaumanislv@gmail.com>
2024-03-27 18:48:43 +0200

    Add theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
@@ -11,19 +10,16 @@ const config = {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		// 3. Append the path to the Skeleton package
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
-		// 4. Append the Skeleton plugin (after other plugins)
-		skeleton
+		skeleton({
+			themes: { preset: ['rocket'] }
+		})
 	]
 } satisfies Config;
 
 export default config;
-						
